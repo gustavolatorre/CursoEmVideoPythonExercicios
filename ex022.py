b49e9167bf5cc3854dc9f313adec3fd55a4a9b64
gustavolatorre <gustavolatorre111@gmail.com>
2021-10-07 22:53:30 -0300

    Exercícios Curso em Vídeo Python

---
+++ ex022.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome completo: ')).strip()
+lista_nome = nome.split()
+print(f'''Analisando seu nome...
+Seu nome em maiúsculo é {nome.upper()}
+Seu nome em minúsculo é {nome.lower()}
+Seu nome tem ao todo {len(nome) - nome.count(' ')} letras
+Seu primeiro nome é {lista_nome[0]} e ele tem {nome.find(' ')} letras''')
