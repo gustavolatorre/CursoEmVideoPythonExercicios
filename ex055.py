17098542b539d4581ea3ab47f86f9c4dc51282eb
gustavolatorre <gustavolatorre111@gmail.com>
2021-10-19 19:12:31 -0300

    Exercícios Curso em Vídeo Python

---
+++ ex055.py
@@ -0,0 +1,16 @@
+maior = 0
+menor = 0
+
+for i in range(1, 6):
+    peso = float(input(f'Dige o peso da {i} pessoa: '))
+    if i == 1:
+        maior = peso
+        menor= peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+
+print(f'''O maior peso lido foi {maior:.1f}Kg
+O menor peso lido foi {menor: .1f}Kg''')
