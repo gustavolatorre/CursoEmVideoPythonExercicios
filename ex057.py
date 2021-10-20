c7beb8a2eb0b6eb4d92aba26c81a7212c3140ae3
gustavolatorre <gustavolatorre111@gmail.com>
2021-10-19 21:22:25 -0300

    Exercícios Curso em Vídeo Python

---
+++ ex057.py
@@ -0,0 +1,6 @@
+sexo = str(input('Informe seu sexo [M/F]: ')).strip().upper()
+
+while sexo not in 'MmFf':
+    sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).strip().upper()
+
+print(f'Sexo {sexo} registrado com sucesso!')
