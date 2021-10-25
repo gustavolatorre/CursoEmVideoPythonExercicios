4940e18886bb19967bdced4b260784722463b20e
gustavolatorre <gustavolatorre111@gmail.com>
2021-10-25 19:46:45 -0300

    Exercícios Curso em Vídeo Python

---
+++ ex077.py
@@ -0,0 +1,7 @@
+palavras = ('borracha', 'aprender', 'chorar', 'falar', 'ajudar', 'programador')
+
+for palavra in palavras:
+    print(f'\nNa palavra {palavra.upper()} temos ', end='')
+    for letra in palavra:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
