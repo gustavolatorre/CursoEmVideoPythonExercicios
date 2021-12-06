27cbf1f974118ad4e82dc5338dbcb3e42d978288
gustavolatorre <gustavolatorre111@gmail.com>
2021-12-06 15:23:25 -0300

    Exercícios Curso em Vídeo Python

--- moeda.py
+++ moeda.py
@@ -20,3 +20,15 @@ def metade(valor, format=False):
 
 def moeda(valor=0, moeda='R$'):
     return f'{moeda}{valor:.2f}'.replace('.', ',')
+
+
+def resumo(valor, pctAumento, pctDiminuir):
+    print('=-'*15)
+    print(f'{"Resumo do valor":^30}')
+    print('=-' * 15)
+    print(f'Preço analisado: \t{moeda(valor)}')
+    print(f'Dobro do preço: \t{dobro(valor, True)}')
+    print(f'Metade do preço: \t{metade(valor, True)}')
+    print(f'{pctAumento}% de aumento: \t{aumentar(valor, pctAumento, True)}')
+    print(f'{pctDiminuir}% de redução: \t{diminuir(valor, pctDiminuir, True)}')
+    print('=-' * 15)
